a7d3f421b1260ecb293a2a840038158f8bfef325
Sadat2021 <Azade.s.it@gmail.com>
2021-12-28 18:49:25 +1100

    show count of shows

--- src/page-components/Category/components/CategoryShows/index.js
+++ src/page-components/Category/components/CategoryShows/index.js
@@ -3,6 +3,7 @@ import Paragraph from 'shared-components/Typography/Paragraph';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { StyledBox, StyledCategoryShows, TextWrapper } from './styled';
+import Header from 'shared-components/Typography/Header';
 
 function CategoryShows({ shows, description }) {
   return (
@@ -10,26 +11,35 @@ function CategoryShows({ shows, description }) {
       <Flex justifyContent="space-between" alignItems="center" flexWrap="wrap">
         <StyledBox>
           {description && (
-          <TextWrapper>
-            <Paragraph text={description} variant="l" transparent />
-          </TextWrapper>
+            <TextWrapper>
+              <Paragraph text={description} variant="l" transparent />
+            </TextWrapper>
           )}
         </StyledBox>
       </Flex>
+      {shows && shows.length > 0 && (
+        <>
+          <Flex pl={2} justifyContent="space-between">
+            <Header text={`${shows.length} Podcasts`} variant="xl" />
+          </Flex>
+        </>
+      )}
     </StyledCategoryShows>
   );
 }
 
 CategoryShows.propTypes = {
-  shows: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string,
-    description: PropTypes.string,
-    images: PropTypes.shape({
-      squareLarge: PropTypes.shape({
-        url: PropTypes.string,
+  shows: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      images: PropTypes.shape({
+        squareLarge: PropTypes.shape({
+          url: PropTypes.string,
+        }),
       }),
-    }),
-  })),
+    })
+  ),
   description: PropTypes.string,
 };
 
